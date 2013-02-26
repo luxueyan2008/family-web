0b4f6775b7a0082b33aa15a71a54ce9b70d9e35c
luxueyan200888 <luxueyan@umeng.com>
2013-02-26 22:12:17 +0800

    add underscore and the route add the commonviewdata

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,21 @@
  * GET home page.
  */
 var user = require('./user')
-  , md = require('./markdown');
+  , md = require('./markdown')
+  , _ = require('underscore');
+var commonViewData;
 module.exports = function (app) {
+	app.get('*',function(req,res,next){
+		commonViewData = {req: req,res: res};
+		next();
+	});
 	app.get('/', function(req, res){
-	  	res.render('index', { title: '山炮哥之家-首页' });
+		res.render('index', _.extend(commonViewData, {title: '山炮哥之家-注册'}));
 	});
 	app.get('/reg',function(req, res){
-		res.render('reg',{ title: '山炮哥之家-注册' });
+		res.render('reg',_.extend(commonViewData, {title: '山炮哥之家-注册'}));
 	});
 	app.get('/users', user.list);
 	app.get('/markdown', md.demo);
 	return app.router;
-}
+};
